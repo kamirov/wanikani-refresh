2a93026fd61a6b01d95bff134ad806edb03789f9
Andrei <andrei.khramtsov@gmail.com>
2024-08-10 21:49:35 -0400

    Update wanikani-refresh.sh

--- wanikani-refresh.sh
+++ wanikani-refresh.sh
@@ -2,23 +2,25 @@
 
 # == Inputs
 prefix=wanikani-wallpaper
-wanikani_api_token="<YOUR_API_KEY>"    # Generate this via WaniKani
-device_num=1                           # Only change this if you're managing multiple wallpapers via the generator
+output_dir="/path/to/output/dir"    
+wanikani_api_token="<my-token>"     # Generate via WaniKani
+device_num=1                        # Only change this if you're managing multiple wallpapers via the generator
 
 # == Implementation
 
 # Delete all previously downloaded wallpapers
-if ls "$(dirname "$0")"/$prefix-* 1> /dev/null 2>&1; then
-    echo "Deleting previous wallpapers..."
-    rm "$(dirname "$0")"/$prefix-*
+if ls "$output_dir/$prefix"-* 1> /dev/null 2>&1; then
+    echo "Deleting previous wallpapers from '$output_dir'..."
+    rm "$output_dir/$prefix"-*
 fi
 
 datetime=$(date -u +"%Y-%m-%dT%H-%M-%SZ")
 output_file="$prefix-$datetime.png"
 user_agent="Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
 generator_url="http://wkw.natural20design.com/?k=$wanikani_api_token&d=$device_num"
+full_output_path="$output_dir/$output_file"
 
 # Fetch the wallpaper
-curl -s -A "$user_agent" -o "$(dirname "$0")/$output_file" $generator_url
+curl -s -A "$user_agent" -o "$full_output_path" $generator_url
 
-echo "Wallpaper saved to $output_file"
+echo "Wallpaper saved to $full_output_path"
