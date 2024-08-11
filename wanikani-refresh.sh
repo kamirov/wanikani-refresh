b7b3b64f8cd1eeb103ccb1b8ca2e5efa09868bbb
Andrei <andrei.khramtsov@gmail.com>
2024-08-10 20:56:03 -0400

    Update wanikani-refresh.sh

--- wanikani-refresh.sh
+++ wanikani-refresh.sh
@@ -2,7 +2,8 @@
 
 # == Inputs
 prefix=wanikani-wallpaper
-wanikani_api_key="<YOUR_API_KEY>"    # Generate this via WaniKani
+wanikani_api_token="<YOUR_API_KEY>"    # Generate this via WaniKani
+device_num=1                         # Only change this if you're managing multiple wallpapers via the generator
 
 # == Implementation
 
@@ -14,8 +15,10 @@ fi
 
 datetime=$(date -u +"%Y-%m-%dT%H-%M-%SZ")
 output_file="$prefix-$datetime.png"
+user_agent="Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
+generator_url="http://wkw.natural20design.com/?k=$wanikani_api_token&d=$device_num"
 
 # Fetch the wallpaper
-curl -s -A "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36" -o "$(dirname "$0")/$output_file" "http://wkw.natural20design.com/?k=$wanikani_api_key&d=1"
+curl -s -A "$user_agent" -o "$(dirname "$0")/$output_file" $generator_url
 
 echo "Wallpaper saved to $output_file"
